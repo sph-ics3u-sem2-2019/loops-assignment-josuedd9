c76df4567bd78a9d68381c3846d58d0697b1adb4
josuedd9 <47532320+josuedd9@users.noreply.github.com>
2019-03-25 11:15:07 -0400

    Update Main.java

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+	System.out.println("Insert number");
+	int num=in.nextInt();
 	}
+
 
 }
