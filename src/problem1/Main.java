c427fcb62e2f0cf1bdd1bd32601518f4ea591639
josuedd9 <47532320+josuedd9@users.noreply.github.com>
2019-03-26 09:20:43 -0400

    Update Main.java

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,9 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		int total=0;
+		int input=0;
 	System.out.println("Insert number");
-	int num=in.nextInt();
+	input=in.nextInt();
+	while (total<=input) {
+		System.out.println(total);
+		total++;
 	}
-
-
+			
+  }
 }
+
