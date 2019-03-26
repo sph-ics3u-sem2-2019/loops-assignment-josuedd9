89a1926c758f9a282f25cd3f9454d8b964425a2f
josuedd9 <47532320+josuedd9@users.noreply.github.com>
2019-03-26 09:51:46 -0400

    Update Main.java

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -5,7 +5,22 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in two numbers. Add up all the values between them
 		Scanner in=new Scanner(System.in);
-
+		int input=0;
+		int input2=0;
+		System.out.println("Insert number here");
+		input=in.nextInt();
+		System.out.println("Insert second number here");
+		input2=in.nextInt();
+		while (input<=input2) {
+			System.out.println(input);
+			input++;
+			
+		}  while (input>=input2) {
+			System.out.println(input);
+			input--;
+		}
+		
+		
 	}
 
 }
