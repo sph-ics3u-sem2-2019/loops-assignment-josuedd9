8af5a319dc4b2532a55d3be7df79a81237d1f073
josuedd9 <47532320+josuedd9@users.noreply.github.com>
2019-03-28 09:55:42 -0400

    Update Main.java

--- src/problem3/Main.java
+++ src/problem3/Main.java
@@ -5,6 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in a word and print out one character per String
 		Scanner in=new Scanner(System.in);
+		String input=0;
+	System.out.println("Read in word");
+	input=in.nextInt();
+	{} for (int counter=input;counter<word.length();counter++) {
+		System.out.println(counter);
 	}
-
+ }
 }
+
+
+
